368f7601d8c3d76860256902fe3348d76fa34eb6
Arnab2002Pal <101279242+Arnab2002Pal@users.noreply.github.com>
2022-08-23 23:29:53 +0530

    Add files via upload

---
+++ MAD_Practical-1_20012011006/kotlin/prac1_2.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var a:Int = 10
+    var b = a.toDouble()
+    var g: String = "80"
+    var f: String = "21.112"
+    var c = Integer.parseInt(g)
+    var d = g.toInt()
+    var e = f.toDouble()
+    println("Integer Value:$a")
+    println("Double Value From Integer:$b")
+    println("String Value:$g")
+    println("Integer Value1 From String:$c")
+    println("Integer Value2 From String:$d")
+    println("Double Value From String:$e")
+}
